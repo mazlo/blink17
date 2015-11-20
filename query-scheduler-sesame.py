1e6b2533ffc5edaea1cbf4699f25d75ab3e2d79f
Matthäus Zloch <matthaeus.zloch@gmail.com>
2015-11-20 17:12:43 +0100

    add python script to have the queries executed

---
+++ query-scheduler-sesame.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# service mysqld stop
+# sync && echo 3 > /proc/sys/vm/drop_caches
+# service mysqld start
+
+# sed -i 's/thread.pool.size=.*/thread.pool.size=15/' application.properties
+# java -jar disco-evaluation-0.0.1-SNAPSHOT.jar
+
+import os
+import time
+
+for tps in ["5","10"]: #,"15","20"]:
+  os.system( "service mysqld stop" );
+  print "dropping caches..";
+  os.system( "sync && echo 3 > /proc/sys/vm/drop_caches" );
+  os.system( "service mysqld start" );
+  time.sleep(5);
+  print "setting up thread pool to "+ tps;
+  os.system( "sed -i 's/thread.pool.size=.*/thread.pool.size="+ tps +"/' application.properties ");
+  print "about to start evaluation..";
+  time.sleep(5);
+  os.system( "java -jar disco-evaluation-0.0.1-SNAPSHOT.jar" );
+
