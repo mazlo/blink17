9037b1f6d16f82b4048033ae7b3ba24b48ea6836
Matthäus Zloch <matthaeus.zloch@gmail.com>
2015-11-24 20:46:54 +0100

    add query-scheduler script for sesame

--- query-scheduler-sesame.py
+++ query-scheduler-sesame.py
@@ -10,12 +10,18 @@
 import os
 import time
 
-for tps in ["5","10"]: #,"15","20"]:
-  os.system( "service mysqld stop" );
+for tps in ["1","5","10","20"]:
+  print "shutting down apache..";
+  os.system( "/home/matthaeus/Projects/apache-tomcat-6.0.43/bin/shutdown.sh" );
+  time.sleep(10);
   print "dropping caches..";
   os.system( "sync && echo 3 > /proc/sys/vm/drop_caches" );
-  os.system( "service mysqld start" );
-  time.sleep(5);
+  print "starting apache..";
+  os.system( "su - matthaeus -c \"JAVA_OPTS=\\\"-Xms6G -Xmx6G\\\" /home/matthaeus/Projects/apache-tomcat-6.0.43/bin/startup.sh\"" );
+  time.sleep(10);
+  print "query to initialize db.."
+  os.system( "curl http://localhost:8080/openrdf-workbench/repositories/in-memory-store/query?action=exec\&queryLn=SPARQL\&query=SELECT%28COUNT%28%3Fz%29AS%3Fc%29WHERE{%3Fx%20%3Fy%20%3Fz}LIMIT10" );
+  time.sleep(10);
   print "setting up thread pool to "+ tps;
   os.system( "sed -i 's/thread.pool.size=.*/thread.pool.size="+ tps +"/' application.properties ");
   print "about to start evaluation..";
