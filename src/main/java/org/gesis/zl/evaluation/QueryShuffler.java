a203e398ecd68d03b98a7d968c4723f176737b7e
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-02-12 13:06:53 +0100

    QueryShuffler -> add FileFilter

--- src/main/java/org/gesis/zl/evaluation/QueryShuffler.java
+++ src/main/java/org/gesis/zl/evaluation/QueryShuffler.java
@@ -2,6 +2,7 @@ package org.gesis.zl.evaluation;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -9,12 +10,71 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QueryShuffler
 {
 	private static final Logger log = LoggerFactory.getLogger( QueryShuffler.class );
+
+	/**
+	 * 
+	 * @param fromFolder
+	 * @return
+	 */
+	public static String[][] readAndShuffle( final String fromFolder, final int multiplyNumberOfQueries )
+	{
+		File[] filenamesList = null;
+
+		filenamesList = getQueries( fromFolder );
+		filenamesList = multiplyNumberOfQueries( filenamesList, multiplyNumberOfQueries );
+
+		Collections.shuffle( Arrays.asList( filenamesList ) );
+
+		String[][] queries = mapQueryNameToQuery( filenamesList );
+
+		return queries;
+	}
+
+	/**
+	 * 
+	 * @param filenamesList
+	 * @return
+	 */
+	private static String[][] mapQueryNameToQuery( File[] filenamesList )
+	{
+		String[][] queries = new String[filenamesList.length][2];
+
+		for ( int i = 0; i < filenamesList.length; i++ )
+		{
+			File queryFile = filenamesList[i];
+
+			queries[i][0] = queryFile.getName();
+
+			BufferedReader reader;
+			try
+			{
+				reader = new BufferedReader( new FileReader( queryFile ) );
+
+				String query = reader.readLine();
+				queries[i][1] = query.trim();
+			}
+			catch ( FileNotFoundException e )
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch ( IOException e )
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return queries;
+	}
 
 	/**
 	 * 
@@ -43,52 +103,7 @@ public class QueryShuffler
 	 * @param fromFolder
 	 * @return
 	 */
-	public static String[][] readAndShuffle( final String fromFolder, final int multiplyNumberOfQueries )
-	{
-		File[] filenamesList = null;
-
-		filenamesList = getQueries( fromFolder );
-		filenamesList = multiplyNumberOfQueries( filenamesList, multiplyNumberOfQueries );
-
-		Collections.shuffle( Arrays.asList( filenamesList ) );
-
-		String[][] queries = new String[filenamesList.length][2];
-
-		for ( int i = 0; i < filenamesList.length; i++ )
-		{
-			File queryFile = filenamesList[i];
-
-			queries[i][0] = queryFile.getName();
-
-			BufferedReader reader;
-			try
-			{
-				reader = new BufferedReader( new FileReader( queryFile ) );
-
-				String query = reader.readLine();
-				queries[i][1] = query.trim();
-			}
-			catch ( FileNotFoundException e )
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			catch ( IOException e )
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		return queries;
-	}
-
-	/**
-	 * 
-	 * @param fromFolder
-	 * @return
-	 */
-	public static File[] getQueries( final String fromFolder )
+	public static File[] getQueries( final String fromFolder, final String... availableQueries )
 	{
 		File queryFolder = new File( fromFolder );
 
@@ -98,7 +113,30 @@ public class QueryShuffler
 			return new File[] {};
 		}
 
-		File[] filenamesList = queryFolder.listFiles();
+		File[] filenamesList = null;
+
+		if ( availableQueries == null || availableQueries.length == 0 )
+		{
+			filenamesList = queryFolder.listFiles();
+		}
+		else
+		{
+			filenamesList = queryFolder.listFiles( new FileFilter()
+			{
+				public boolean accept( File currentFile )
+				{
+					for ( String filename : availableQueries )
+					{
+						if ( StringUtils.equals( currentFile.getName(), filename ) )
+							return true;
+
+						continue;
+					}
+
+					return false;
+				}
+			} );
+		}
 
 		if ( filenamesList.length == 0 )
 		{
