c4593643fac7e4a386ef66fa5a1d444de78cb673
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-03-04 11:50:44 +0100

    add threads 15 for sesame evaluation

--- query-scheduler-sesame.py
+++ query-scheduler-sesame.py
@@ -10,14 +10,14 @@
 import os
 import time
 
-for tps in ["1","5","10","20"]:
+for tps in ["1","5","10","15","20"]:
   print "shutting down apache..";
-  os.system( "/home/matthaeus/Projects/apache-tomcat-6.0.43/bin/shutdown.sh" );
+  os.system( "sudo -u matthaeus -H /home/matthaeus/Projects/apache-tomcat-6.0.43/bin/shutdown.sh" );
   time.sleep(10);
   print "dropping caches..";
   os.system( "sync && echo 3 > /proc/sys/vm/drop_caches" );
   print "starting apache..";
-  os.system( "su - matthaeus -c \"JAVA_OPTS=\\\"-Xms6G -Xmx6G\\\" /home/matthaeus/Projects/apache-tomcat-6.0.43/bin/startup.sh\"" );
+  os.system( "sudo -u matthaeus -H JRE_HOME=\"/usr/lib/java/jdk1.7.0_75\" JAVA_OPTS=\"-Xms6G -Xmx6G\" /home/matthaeus/Projects/apache-tomcat-6.0.43/bin/startup.sh" );
   time.sleep(10);
   print "query to initialize db.."
   os.system( "curl http://localhost:8080/openrdf-workbench/repositories/in-memory-store/query?action=exec\&queryLn=SPARQL\&query=SELECT%28COUNT%28%3Fz%29AS%3Fc%29WHERE{%3Fx%20%3Fy%20%3Fz}LIMIT10" );
