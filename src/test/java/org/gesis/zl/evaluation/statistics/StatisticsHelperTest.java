280634eb3475df316e136ba9d59036670ec0dcfb
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-04-01 18:05:23 +0200

    StatisticsHelper -> introduce apache.math to use DescriptiveStatistics

--- src/test/java/org/gesis/zl/evaluation/statistics/StatisticsHelperTest.java
+++ src/test/java/org/gesis/zl/evaluation/statistics/StatisticsHelperTest.java
@@ -11,41 +11,33 @@ import com.google.common.collect.Multimap;
 public class StatisticsHelperTest
 {
 
+	Multimap<String, Long> results = ArrayListMultimap.create();
+
 	@Before
 	public void init()
 	{
-
+		results.put( "q1", 30l );
+		results.put( "q1", 15l );
+		results.put( "q1", 40l );
+		results.put( "q1", 20l );
+		results.put( "q1", 400l );
+		results.put( "q1", 5l );
+		results.put( "q2", 30l );
+		results.put( "q2", 30l );
+		results.put( "q2", 3l );
+		results.put( "q2", 30l );
+		results.put( "q2", 20l );
 	}
 
 	@Test
 	public void printDetails() throws IOException
 	{
-		Multimap<String, Long> results = ArrayListMultimap.create();
-
-		results.put( "q1", 30l );
-		results.put( "q1", 15l );
-		results.put( "q1", 40l );
-		results.put( "q1", 5l );
-		results.put( "q2", 30l );
-		results.put( "q2", 30l );
-		results.put( "q2", 20l );
-
 		StatisticsHelper.printDetails( "target/results_test.csv", results );
 	}
 
 	@Test
 	public void printStatistics() throws IOException
 	{
-		Multimap<String, Long> results = ArrayListMultimap.create();
-
-		results.put( "q1", 30l );
-		results.put( "q1", 15l );
-		results.put( "q1", 40l );
-		results.put( "q1", 5l );
-		results.put( "q2", 30l );
-		results.put( "q2", 30l );
-		results.put( "q2", 20l );
-
 		StatisticsHelper.printStatistics( "target/statistics_test.csv", results );
 	}
 }
