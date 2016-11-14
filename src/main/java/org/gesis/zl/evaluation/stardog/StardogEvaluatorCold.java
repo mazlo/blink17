01d764602790f9bcf459cc388bbec075646ef195
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-11-14 16:11:52 +0100

    cold evaluation, add Stardog implementation

---
+++ src/main/java/org/gesis/zl/evaluation/stardog/StardogEvaluatorCold.java
@@ -0,0 +1,71 @@
+package org.gesis.zl.evaluation.stardog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+import org.gesis.zl.evaluation.Evaluator;
+import org.gesis.zl.evaluation.service.EvaluationProperties;
+import org.gesis.zl.evaluation.statistics.StatisticsHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.Futures;
+
+/**
+ * @author matthaeus
+ * 
+ */
+public class StardogEvaluatorCold extends Evaluator
+{
+	private static Logger log = LoggerFactory.getLogger( StardogEvaluatorCold.class );
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gesis.zl.evaluation.Evaluator#execute()
+	 */
+	@Override
+	public Multimap<String, Long> execute() throws InterruptedException
+	{
+		EvaluationProperties props = getEvaluationProperties();
+		String[][] queriesToExecute = getQueries();
+
+		List<Future<Long>> listOfWorkers = new ArrayList<Future<Long>>();
+
+		int totalExecutions = queriesToExecute.length;
+
+		// start so many threads a there are queries
+		for ( int i = 0; i < totalExecutions; i++ )
+		{
+			if ( queriesToExecute[i][1].length() == 0 )
+			{
+				listOfWorkers.add( Futures.immediateFuture( 0l ) );
+				continue;
+			}
+
+			this.prepareEvaluation();
+
+			try
+			{
+				Callable<Long> queryExecution = new StardogQueryExecutor( props.getDbUrl() + props.getDbName(), queriesToExecute[i] );
+
+				// execute
+				Future<Long> submitedWorker = Futures.immediateFuture( queryExecution.call() );
+
+				// remember
+				listOfWorkers.add( submitedWorker );
+			}
+			catch ( Exception e )
+			{
+				log.error( e.getMessage() );
+				continue;
+			}
+		}
+
+		return StatisticsHelper.collectResults( listOfWorkers, queriesToExecute );
+	}
+
+}
